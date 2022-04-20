546a25bb059d6e2fe9648edc8ac3def91bf25ca6
dianaguzman <dguzman2@mail.umw.edu>
2022-04-20 12:06:14 -0400

    Unresolved

--- Trivia.java
+++ Trivia.java
@@ -6,21 +6,27 @@ public class Trivia {
     private int newCardOrNot;
     // private ArrayList<Trivia> triviaArrayList;
 
+    //Constructor for Trivia Class
     public Trivia(String trivQuestion, int newCardOrNot ) {
         this.trivQuestion = trivQuestion;
         this.newCardOrNot = newCardOrNot;
     }
 
     //getter and setter
+
+    //Setter for trivQuestion
     public void settrivQuestion(String trivQuestion){
         this.trivQuestion = trivQuestion;
     }
+    //Setter for newCardOrNot
     public void setnewCardOrNot(int newCardOrNot ){
         this.newCardOrNot = newCardOrNot;
     }
+    //Getter for trivQuestion
     public String gettrivQuestion(){
         return this.trivQuestion;
     }
+    //Setter for newCardOrNot
     public int getnewCardOrNot() {
         return this.newCardOrNot;
     }
